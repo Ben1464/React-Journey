e1c9b1425f6989f1a354870d6c1fce9c1ea77570
Benson Mwangi <kimaniben@gmail.com>
2023-11-19 05:29:40 -0500

    Initial commit

---
+++ src/components/Greetings.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Greetings = ()=> <h1>Hello team</h1>
+
+export default Greetings;
---
+++ src/components/Message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Message = () => <p>React is not hard at all it all takes dedication</p>
+
+export default Message;
+
+
