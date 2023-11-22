771c828760f2b09617d93b5e92be42083882854c
Benson Mwangi <kimaniben@gmail.com>
2023-11-22 02:04:42 -0500

    props

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const Message = () => <p>React is not hard at all it all takes dedication</p>
+const Message = () => { <p>React is not hard at all it all takes dedication</p>
 
+}
 export default Message;
 
 
