80fcc70291a27243816aaeb33e36972a51ae73f9
Benson Mwangi <kimaniben@gmail.com>
2023-11-26 14:33:58 -0500

    Updates on contacts

--- src/components/Contants.js
+++ src/components/Contants.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Students from './Student'
 function contacts (props){
     return (
         <div>
             <h1>Information for individual student</h1>
             <h2>kindly provide with your{props.cellPhone} your email {props.email}and your home box {props.box}</h2>
+            <p>Through the props {props.name}, {props.occupationName}</p>
         </div>
     )
+    
 }
 export default contacts;
